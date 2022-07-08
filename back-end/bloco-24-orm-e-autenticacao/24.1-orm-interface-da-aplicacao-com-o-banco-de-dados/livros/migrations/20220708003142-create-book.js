f6118657290b8f2b233a2b66c73b216cff8c902b
Maju Domenciano <juliadomenciano@hotmail.com>
2022-07-07 21:40:23 -0300

    ex 1

---
+++ back-end/bloco-24-orm-e-autenticacao/24.1-orm-interface-da-aplicacao-com-o-banco-de-dados/livros/migrations/20220708003142-create-book.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      author: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      pageQuantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        
+
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('books');
+  }
+};
